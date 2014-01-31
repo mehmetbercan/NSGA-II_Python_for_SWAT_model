b94d139b3a6be9cc3c4d4833f38e5bc8d2854156
Mehmet <mehmetbercan@gmail.com>
2014-01-31 14:42:43 -0500

    Example Model Added
    This is added to demonstrate how the code works with SWAT input files.
    The SWAT input files are added in to the main folder and Backup folder
    under main folder. To run this code just need to run
    "nsga-II_Bekele_Cloud.py".

---
+++ ExampleModel/Backup/001890007.sol
@@ -0,0 +1,22 @@
+ .Sol file Watershed HRU:1880 Subbasin:189 HRU:7 Luse:URMD Soil: NC052 Slope: 5-10 2/7/2013 12:00:00 AM ArcSWAT 2012.10_1.5
+ Soil Name: MAYODAN
+ Soil Hydrologic Group: B
+ Maximum rooting depth(m) : 1524.00
+ Porosity fraction from which anions are excluded: 0.500
+ Crack volume potential of soil: 0.500
+ Texture 1                : SL-SCL-SC-VAR
+ Depth                [mm]:      304.80      457.20     1193.80     1524.00
+ Bulk Density Moist [g/cc]:        1.52        1.35        1.40        1.50
+ Ave. AW Incl. Rock Frag  :        0.11        0.12        0.11        0.10
+ Ksat. (est.)      [mm/hr]:       74.00       16.00        3.40      500.00
+ Organic Carbon [weight %]:        0.73        0.29        0.15        0.05
+ Clay           [weight %]:       12.50       30.00       47.50       15.00
+ Silt           [weight %]:       19.65       14.56        2.78       30.00
+ Sand           [weight %]:       67.85       55.44       49.72       55.00
+ Rock Fragments   [vol. %]:        6.51        1.80        3.23       20.00
+ Soil Albedo (Moist)      :        0.06        0.13        0.17        0.21
+ Erosion K                :        0.24        0.32        0.28        0.00
+ Salinity (EC, Form 5)    :        0.00        0.00        0.00        0.00
+ Soil pH                  :        0.00        0.00        0.00        0.00
+ Soil CACO3               :        0.00        0.00        0.00        0.00
+                              
