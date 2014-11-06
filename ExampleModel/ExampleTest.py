92607c4a7e250784beb0fa7a45d423dae8f9db0a
Mehmet <mehmetbercan@gmail.com>
2014-11-06 13:26:20 -0500

    Bug removed from ExampleTest.py

--- ExampleModel/ExampleTest.py
+++ ExampleModel/ExampleTest.py
@@ -12,8 +12,7 @@ ns.DetermineInitialPopulation()
 TotalNumGenerations = ns.gener
 i=0
 while i < TotalNumGenerations:
-    ns.Selection(); #select solutions from old population to mate population
-    ns.old_pop_ptr = copy.deepcopy(ns.new_pop_ptr); #Now, old population becames new population before new population changes
+    ns.Selection(); #select solutions
     ns.Crossover() #New population changes based on mate population
     ns.Mutation()  #New population mutates
     nsga2funcs.decode(ns.new_pop_ptr, ns.vlen, ns.lim_b); #Turn binary calibration parameters into normal numbers
@@ -22,8 +21,9 @@ while i < TotalNumGenerations:
     nsga2funcs.decode(ns.mate_pop_ptr, ns.vlen, ns.lim_b); 
     #print
     nsga2funcs.report(ns.old_pop_ptr,ns.mate_pop_ptr,i+1,ns.gener,ns.SWATdir,ns.ncross,ns.nmut);
-    #copy mate to new population
+    #copy
     ns.new_pop_ptr = copy.deepcopy(ns.mate_pop_ptr);
+    ns.old_pop_ptr = copy.deepcopy(ns.new_pop_ptr);
     i+=1
 
 
